9ca0d8aa54a0d5cf64d5cca9b809404b91a27a2c
橋本羽海 <>
2023-03-14 21:15:06 +0900

    test

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+// var canvas1 = document.getElementById('l-canvas');
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var raf;
 
 var ball = {
     x: 50,
-    y: 50,
-    vx: 0.2,
-    vy: 0.1,
-    radius: 10,
+    y: 100,
+    vx: 4,
+    vy: 3,
+    radius: 30,
     color: 'blue',
     draw: function() {
         ctx.beginPath();
@@ -18,21 +19,14 @@ var ball = {
     }
 };
 
-// var ball = {
-//     x: 50,
-//     y: 50,
-//     vx: 3,
-//     vy: 2,
-//     radius: 3,
-//     color: '#006400',
-//     draw: function() {
-//         ctx.beginPath();
-//         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-//         ctx.closePath();
-//         ctx.fillStyle = this.color;
-//         ctx.fill();
-//     }
-//     };
+var w = $('.l-canvas').width();
+var h = $('.l-canvas').height();
+$('#canvas').attr('width', w);
+$('#canvas').attr('height', h);
+
+
+
+
 
 function draw() {
     ctx.clearRect(0,0, canvas.width, canvas.height);
